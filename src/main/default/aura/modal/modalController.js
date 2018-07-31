c37f1c0ff141e65e3e5b1aea2e33d2644e5eb2be
pozil <>
2018-07-31 12:37:11 +0200

    Applied ES6 syntax

--- src/main/default/aura/modal/modalController.js
+++ src/main/default/aura/modal/modalController.js
@@ -4,32 +4,32 @@
 	},
 
 	openModalAlert : function(component, event, helper) {
-		var params = event.getParam('arguments');
+		const params = event.getParam('arguments');
 		// Set title
-		component.set("v.title", params.title);
+		component.set('v.title', params.title);
 		// Add body
 		$A.createComponent(
-			"aura:text", {'value': params.message},
-			function(newText, status, errorMessage){
-				if (status === "SUCCESS") {
-					var body = component.get("v.body");
+			'aura:text', {value: params.message},
+			(newText, status, errorMessage) => {
+				if (status === 'SUCCESS') {
+					const body = component.get('v.body');
 					body.push(newText);
-					component.set("v.body", body);
+					component.set('v.body', body);
 				}
 			}
 		);
 		// Add 'ok' button
 		$A.createComponent(
-			"lightning:button", {
-				'label': 'Ok',
-				'variant': 'brand',
-				'onclick': component.getReference('c.destroyModal')
+			'lightning:button', {
+				label: 'Ok',
+				variant: 'brand',
+				onclick: component.getReference('c.destroyModal')
 			},
-			function(newButton, status, errorMessage){
-				if (status === "SUCCESS") {
-					var footer = component.get("v.footer");
+			(newButton, status, errorMessage) => {
+				if (status === 'SUCCESS') {
+					const footer = component.get('v.footer');
 					footer.push(newButton);
-					component.set("v.footer", footer);
+					component.set('v.footer', footer);
 				}
 			}
 		);
--- src/main/default/aura/modal/modalHelper.js
+++ src/main/default/aura/modal/modalHelper.js
@@ -1,11 +1,11 @@
 ({
 	showModal : function(component) {
-		var modalContainer = component.find("modalContainer");
-		$A.util.removeClass(modalContainer, "slds-hide");
+		const modalContainer = component.find('modalContainer');
+		$A.util.removeClass(modalContainer, 'slds-hide');
 	},
 
 	hideModal : function(component) {
-		var modalContainer = component.find("modalContainer");
-		$A.util.addClass(modalContainer, "slds-hide");
+		const modalContainer = component.find('modalContainer');
+		$A.util.addClass(modalContainer, 'slds-hide');
 	}
 })
